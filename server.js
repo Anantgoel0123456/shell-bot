df391f808f2d094534bb41d8793c7c5e01673f80
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2021-09-26 09:42:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -503,14 +503,6 @@ bot.command("help", function (msg, reply, next) {
 
 app.set('port', (process.env.PORT || 5000));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
 // FIXME: add inline bot capabilities!
 // FIXME: possible feature: restrict chats to UIDs
 // FIXME: persistence
